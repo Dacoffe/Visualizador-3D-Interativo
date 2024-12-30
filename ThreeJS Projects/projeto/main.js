0eb078210cc25dd8c114c2f857a8f64087d730dc
and123456222 <sandreununesb@gmail.com>
2024-12-30 23:28:13 +0000

    main

--- ThreeJS Projects/projeto/main.js
+++ ThreeJS Projects/projeto/main.js
@@ -1,70 +1,66 @@
 import * as THREE from 'three'; 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-
-
-
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+//MUDAR VARIAVEIS E COMENTARIOS
 
+//MUDAR VARIAVEIS E COMENTARIOS
 let cena = new THREE.Scene() 
 
-//METER HORIZONTAL 180 GRAUS
-function resetarControles() {
-  // Resetando os valores dos botões de animação
+//Reset nas opcoes de animação, no modelo default e nas opções de luz para que não fiquem as mesmas opções quando há refresh da página
+function resetarOpcoes() {
+  
   document.getElementById('btn_playSupportJoint').disabled = false;
   document.getElementById('btn_playLongArm').disabled = false;
   document.getElementById('btn_playShortArm').disabled = false;
   document.getElementById('btn_playArmToAbajur').disabled = false;
   document.getElementById('btn_playAbajur').disabled = false;
 
-  // Resetando o menu suspenso para o valor padrão
-  document.getElementById('btn_changeModel').selectedIndex = 0; // Reseta para o valor padrão (opção 2)
+  document.getElementById('btn_changeModel').selectedIndex = 0; 
 
-  // Resetando os controles deslizantes (se houver)
-  document.getElementById('intensidadeLuz').value = 0;  // Intensity slider reset to default
-  document.getElementById('corLuz').value = "#ffff00";  // Color picker reset to default
+  resetarLuzes();
 
-  // Resetando o menu de loop para a opção padrão (Repetir)
-  document.getElementById('menu_loop').selectedIndex = 1;  // Reseta para a opção "Repetir"
+   
 }
-
-// Chame essa função sempre que a página for carregada ou após o refresh
-resetarControles();
+function resetarLuzes(){
+  document.getElementById('intensidadeLuz').value = 0;  
+  document.getElementById('corLuz').value = "#ffff00"; 
+  document.getElementById('menu_loop').selectedIndex = 1; 
+}
+//Função utilizada quando a página tem refresh
+resetarOpcoes();
+//Criação da câmara
 let camara = new THREE.PerspectiveCamera( 70, 800 / 600, 0.1, 500 ) 
+//posição inicial da câmara
 camara.position.set( 8, 0, 7)
-
+//posição onde a câmara foca
 camara.lookAt( 0, 0, 0 )
 
 let meuCanvas = document.getElementById( 'meuCanvas')
 let renderer = new THREE.WebGLRenderer( { canvas: meuCanvas } )
+//aplica tamanho da janela
 renderer.setSize( 800, 600 )
+//aumentar número de pixeis
 renderer.setPixelRatio(window.devicePixelRatio * 2)
+// luzes podem causar sombras
 renderer.shadowMap.enabled = true
-
+//criar controlos de movimento
 let controlos = new OrbitControls( camara, renderer.domElement )
 
+//bloqueia o moviento vertical para que só se mova horizontal
+controlos.minPolarAngle = Math.PI / 2;
+controlos.maxPolarAngle = Math.PI / 2; 
 
-controlos.minPolarAngle = Math.PI / 2; // Ângulo mínimo (horizontal)
-controlos.maxPolarAngle = Math.PI / 2; // Ângulo máximo (horizontal)
-
-controlos.enableDamping = true; // Efeito suave
+controlos.enableDamping = true; 
 controlos.dampingFactor = 0.1;
 controlos.enablePan = false;
 
-renderer.render( cena, camara )
 
-//let grelha = new THREE.GridHelper() 
-
-//cena.add( grelha )
 
 let delta = 0; // tempo desde a última atualização
 let relogio = new THREE.Clock()
 let latencia_minima = 1 / 60; // tempo mínimo entre cada atualização
 
 let misturador = new THREE.AnimationMixer(cena)
-
-
-
-
 
 function animar() 
 { 
@@ -79,20 +75,20 @@ function animar()
     
   renderer.render( cena, camara ) 
 
-  
-
   delta = delta % latencia_minima
 }
 
 animar()
-
-let bulbLight, spotLight;
+//cria variaveis de luz
+let PontoLuz, ProjetorLuz;
 
 let carregador = new GLTFLoader() ;
-let modeloAtual; // Variável para armazenar o modelo atual
+//cria variavel a identificar modelo
+let modeloAtual; 
+
 
 function carregarModelo(caminhoModelo) {
-  // Remove o modelo atual da cena, se existir
+  // Remove o modelo atual 
   if (modeloAtual) {
     cena.remove(modeloAtual);
 
@@ -104,100 +100,133 @@ carregador.load(
   caminhoModelo,
   function ( gltf ) 
   { 
-    modeloAtual = gltf.scene;
-
-
-        
+    modeloAtual = gltf.scene;  
     cena.add(modeloAtual);
-
-
-    // Remover animações anteriores
     misturador = new THREE.AnimationMixer(modeloAtual); 
 
-    const lightBulb = modeloAtual.getObjectByName('S_LightBulb');
-    if (lightBulb) {
-        bulbLight = new THREE.PointLight('yellow', 0, 1); // Luz amarela, intensidade 10, alcance 10
-        bulbLight.position.set(0, 0.8, 0); // Altere a posição, se necessário
-        lightBulb.add(bulbLight);
-
+    const lampada = modeloAtual.getObjectByName('S_LightBulb');
+    if (lampada) {
+        //Adiciona luz de Ponto com cor, intensidade e alcance
+        PontoLuz = new THREE.PointLight('yellow', 0, 1); 
+        //posição que a luz está centrada
+        PontoLuz.position.set(0, 0.8, 0); 
+        lampada.add(PontoLuz);
        
+        //adiciona luz de projeção com cor, intensidade, alcance e ângulo de abertura(30 graus)
+        ProjetorLuz = new THREE.SpotLight('yellow', 0, 100, Math.PI / 6); 
+        //posição da luz
+        ProjetorLuz.position.set(0, -0.6, 0); 
+        // Direção que a luz aponta
+        ProjetorLuz.target.position.set(0, 10, 0);
+        lampada.add(ProjetorLuz.target);
+        lampada.add(ProjetorLuz);
 
-       spotLight = new THREE.SpotLight('yellow', 0, 100, Math.PI / 6); // Cor branca, intensidade 5, alcance 20, ângulo de 30° (PI/6)
-    
-    // Posicionando a luz no centro do objeto
-    spotLight.position.set(0, -0.6, 0); // Posição no centro local do lightBulb
-
-    // Direcionando a luz na direção do eixo Y positivo
-    spotLight.target.position.set(0, 10, 0); // Alvo em direção ao eixo Y positivo (ajuste conforme necessário)
-    
-    // Adicionando o alvo da luz como filho do lightBulb
-    lightBulb.add(spotLight.target);
-
-    // Adicionando a luz ao objeto lightBulb
-    lightBulb.add(spotLight);
-
-    
-
-    console.log('SpotLight adicionada ao objeto "S_LightBulb".');
-
-        
+        console.log('Luzes adicionadas ao objeto "S_LightBulb".');
     } else {
-        console.warn('Objeto "S_LightBulb" não encontrado no modelo.');
+        console.warn('Objeto "S_LightBulb" não encontrado.');
     }
 
-    // Configuração das animações do novo modelo
-    const animacoes = {
+    // Configuração das animações 
+    const animacoesArray = {
       SupportJoint: THREE.AnimationClip.findByName(gltf.animations, 'Su'),
       LongArm: THREE.AnimationClip.findByName(gltf.animations, 'L'),
       ShortArm: THREE.AnimationClip.findByName(gltf.animations, 'Sh'),
       ArmToAbajur: THREE.AnimationClip.findByName(gltf.animations, 'Ata'),
       Abajur: THREE.AnimationClip.findByName(gltf.animations, 'A')
     };
-
-    const acoes = {};
-    Object.keys(animacoes).forEach(nome => {
-      if (animacoes[nome]) {
-        acoes[nome] = misturador.clipAction(animacoes[nome]);
+    //acoesRegistos garante animaçoes em conjunto ao registá-las
+    const acoesRegistos = {};
+    Object.keys(animacoesArray).forEach(nome => {
+      if (animacoesArray[nome]) {
+        acoesRegistos[nome] = misturador.clipAction(animacoesArray[nome]);
       }
     });
 
-    // Atualizar os botões para o novo modelo
-    document.getElementById('btn_playSupportJoint').onclick = () => acoes.SupportJoint?.reset().play();
-    document.getElementById('btn_playLongArm').onclick = () => acoes.LongArm?.reset().play();
-    document.getElementById('btn_playShortArm').onclick = () => acoes.ShortArm?.reset().play();
-    document.getElementById('btn_playArmToAbajur').onclick = () => acoes.ArmToAbajur?.reset().play();
-    document.getElementById('btn_playAbajur').onclick = () => acoes.Abajur?.reset().play();
-
-    // Pausar, parar e inverter animações para o novo modelo
+    //Opções de animação
+    document.getElementById('btn_playSupportJoint').onclick = () => {
+      if (acoesRegistos.SupportJoint) {
+        acoesRegistos.SupportJoint.reset().play();
+      }
+    };
+    
+    document.getElementById('btn_playLongArm').onclick = () => {
+      if (acoesRegistos.LongArm) {
+        acoesRegistos.LongArm.reset().play();
+      }
+    };
+    
+    document.getElementById('btn_playShortArm').onclick = () => {
+      if (acoesRegistos.ShortArm) {
+        acoesRegistos.ShortArm.reset().play();
+      }
+    };
+    
+    document.getElementById('btn_playArmToAbajur').onclick = () => {
+      if (acoesRegistos.ArmToAbajur) {
+        acoesRegistos.ArmToAbajur.reset().play();
+      }
+    };
+    
+    document.getElementById('btn_playAbajur').onclick = () => {
+      if (acoesRegistos.Abajur) {
+        acoesRegistos.Abajur.reset().play();
+      }
+    };
+    
+      // Pausa animação
+      //acao é um dos registos unicos de acoesRegistos
     document.getElementById('btn_pause').onclick = () => {
-      Object.values(acoes).forEach(acao => {
-        if (acao) acao.paused = !acao.paused;
+      Object.values(acoesRegistos).forEach(acao => {
+        if (acao) {
+          acao.paused = !acao.paused;
+        }
       });
     };
-
+    //Para animação
     document.getElementById('btn_stop').onclick = () => {
-      Object.values(acoes).forEach(acao => {
-        if (acao) acao.stop();
+      Object.values(acoesRegistos).forEach(acao => {
+        if (acao) {
+          acao.stop();
+        }
       });
     };
-
+    //Reversa animação
     document.getElementById('btn_reverse').onclick = () => {
-      Object.values(acoes).forEach(acao => {
-        if (acao) acao.timeScale *= -1;
+      Object.values(acoesRegistos).forEach(acao => {
+        if (acao) {
+          acao.timeScale *= -1;
+        }
       });
     };
 
-    // Configuração do loop
+
+    // Selecionar loop da animação
     document.getElementById('menu_loop').onchange = (event) => {
-      const valor = parseInt(event.target.value);
-      const loopModes = [THREE.LoopOnce, THREE.LoopRepeat, THREE.LoopPingPong];
-      const selectedLoopMode = loopModes[valor - 1];
-      Object.values(acoes).forEach(acao => {
-        if (acao) acao.setLoop(selectedLoopMode);
+      let selectedLoopMode;
+    
+      switch (parseInt(event.target.value)) {
+        case 1:
+          selectedLoopMode = THREE.LoopOnce;
+          break;
+        case 2:
+          selectedLoopMode = THREE.LoopRepeat;
+          break;
+        case 3:
+          selectedLoopMode = THREE.LoopPingPong;
+          break;
+        default:
+          console.warn('Modo de loop selecionado inválido');
+          return; 
+      }
+    
+      Object.values(acoesRegistos).forEach(acao => {
+        if (acao){
+           acao.setLoop(selectedLoopMode);
+        }
       });
     };
-
-    // Ativar sombras para o novo modelo
+    
+    // Ativa sombras
     modeloAtual.traverse((element) => {
       if (element.isMesh) {
         element.castShadow = true;
@@ -205,109 +234,101 @@ carregador.load(
       }
     });
   
- // Certifique-se de fechar adequadamente
 console.log('Modelo carregado:', caminhoModelo);
 
 }, undefined, function (error) {
   console.error(error);
 });
-
 }
 
+//muda o modelo com abajures diferentes
 document.getElementById('btn_changeModel').addEventListener('change', () => {
-  const valor = parseInt(event.target.value);
-  switch (valor) {
+  const valorModelo = parseInt(event.target.value);
+  switch (valorModelo) {
     case 1:
+      //carrega modelo com as animações
+      //Abajur preto
       carregarModelo('A.glb');
-      document.getElementById('corLuz').value = "#ffff00";
-      document.getElementById('intensidadeLuz').value = 0;
-      
+      resetarLuzes();
       break;
     case 2:
-      
+      //Abajur castanho
       carregarModelo('Acastanho.glb');
-      document.getElementById('corLuz').value = "#ffff00";
-      document.getElementById('intensidadeLuz').value = 0;
+      resetarLuzes();
       break;
     case 3:
+      //Abajur dourado
       carregarModelo('Adourado.glb');
-      document.getElementById('corLuz').value = "#ffff00";
-      document.getElementById('intensidadeLuz').value = 0;
-      
+      resetarLuzes();
       break;
     default:
       carregarModelo('A.glb');
-      document.getElementById('corLuz').value = "#ffff00";
-      document.getElementById('intensidadeLuz').value = 0;
+      resetarLuzes();
   }
 });
-
-
-
-
+//carrega modelo inicialmente
 carregarModelo('A.glb');
 
-
-// Atualizar a intensidade da luz
+//Ajusta a intensidade da luz
 document.getElementById('intensidadeLuz').addEventListener('input', (event) => {
   const intensidade = event.target.value;
-  if (bulbLight) {
-    bulbLight.intensity = intensidade / 10; // Ajusta a intensidade da luz (ajustando o valor)
+  if (PontoLuz) {
+    PontoLuz.intensity = intensidade; 
   }
-  if (spotLight) {
-    spotLight.intensity = intensidade / 10; // Ajusta a intensidade da luz spot
+  if (ProjetorLuz) {
+    ProjetorLuz.intensity = intensidade;
   }
 });
 
-// Atualizar a cor da luz
+// Ajusta a cor das luzes
 document.getElementById('corLuz').addEventListener('input', (event) => {
   const cor = event.target.value;
-  if (bulbLight) {
-    bulbLight.color.set(cor); // Atualiza a cor da luz de ponto
+  if (PontoLuz) {
+    PontoLuz.color.set(cor); 
   }
-  if (spotLight) {
-    spotLight.color.set(cor); // Atualiza a cor da luz spot
+  if (ProjetorLuz) {
+    ProjetorLuz.color.set(cor); 
   }
 });
 
-// Define o fundo da cena como branco
-cena.background = new THREE.Color('blue');
+// Define a cor do fundo
+cena.background = new THREE.Color('lightgray');
 
-// Adiciona uma luz ambiente para iluminar uniformemente
-const luzAmbiente = new THREE.AmbientLight('white', 0.1); // Intensidade de 0.5
+// luz ambiente
+const luzAmbiente = new THREE.AmbientLight('white', 0.5); 
 cena.add(luzAmbiente);
 
+//Outra luz ponto que ilumina em todas as direções
+const luzPontoAmbiente = new THREE.PointLight( "white" ) 
+luzPontoAmbiente.position.set( 0, 9, 3) 
+luzPontoAmbiente.intensity= 50
+luzPontoAmbiente.castShadow = true
 
+//resolução das sombras
+luzPontoAmbiente.shadow.mapSize.width = 2048;
+luzPontoAmbiente.shadow.mapSize.height = 2048;
+//limite de distância de renderização de sombras
+luzPontoAmbiente.shadow.camera.near = 0.5;
+luzPontoAmbiente.shadow.camera.far = 500;
+cena.add( luzPontoAmbiente ) 
 
-// Ajusta a luz pontual
- // Reduz a intensidade da luz pontual
-
-const luzPonto = new THREE.PointLight( "white" ) 
-luzPonto.position.set( 10, 10, 0) 
-luzPonto.intensity= 1
-luzPonto.castShadow = true
-
-luzPonto.shadow.mapSize.width = 2048;
-luzPonto.shadow.mapSize.height = 2048;
-
-luzPonto.shadow.camera.near = 0.5;
-luzPonto.shadow.camera.far = 500;
-
-
-cena.add( luzPonto ) 
-
-
+//cria uma luz direcional
 const luzDirecional = new THREE.DirectionalLight( "white" );
-luzDirecional.position.set( 3, 2, 0 ); //aponta na direção de (0, 0, 0)
-luzDirecional.intensity= 1
+luzDirecional.position.set( 0, 8, 0 ); //aponta na direção de (0, 0, 0)
+luzDirecional.intensity= 3
 cena.add( luzDirecional );
 
-
-
-//Function to change texture
-
-
-
+//cria outra spot luz
+const luzSpotAmbiente = new THREE.SpotLight("white", 250, 1000, Math.PI /4);
+luzSpotAmbiente.position.set(0, 8, 3); 
+luzSpotAmbiente.target.position.set(0, -8, 3);
+luzSpotAmbiente.castShadow = true;
+luzSpotAmbiente.shadow.mapSize.width = 2048;
+luzSpotAmbiente.shadow.mapSize.height = 2048;
+luzSpotAmbiente.shadow.camera.near = 0.5; 
+luzSpotAmbiente.shadow.camera.far = 200; 
+cena.add( luzSpotAmbiente.target);
+cena.add( luzSpotAmbiente);
 
 console.log(cena);
 
